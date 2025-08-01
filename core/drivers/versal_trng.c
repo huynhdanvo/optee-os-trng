1157cda40d2587b968aa1c927b664618680d6f53
huynhdanvo <dan.vo@provenrun.com>
2025-08-01 14:46:00 +0200

    Fix checkpatch.sh
    Signed-off-by: huynhdanvo <dan.vo@provenrun.com>

--- core/drivers/versal_trng.c
+++ core/drivers/versal_trng.c
@@ -706,8 +706,8 @@ static TEE_Result trng_reseed_internal_nodf(struct versal_trng *trng,
 		persmask = TRNG_CTRL_PERSODISABLE_DEFVAL;
 	}
 
-	trng_write32_v2(trng->cfg.addr + TRNG_CTRL, 
-		TRNG_CTRL_PERSODISABLE_MASK | TRNG_CTRL_PRNGSTART_MASK, 
+	trng_write32_v2(trng->cfg.addr + TRNG_CTRL,
+		TRNG_CTRL_PERSODISABLE_MASK | TRNG_CTRL_PRNGSTART_MASK,
 		persmask);
 	/* DRNG Mode */
 	if (eseed != NULL) {
